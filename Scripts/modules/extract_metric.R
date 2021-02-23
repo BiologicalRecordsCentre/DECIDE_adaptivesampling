ab8fd33c2a5aaf14af4c7045d69e59065f36acd1
Thomas Mondain-Monval <thoval@ceh.ac.uk>
2021-02-23 10:02:35 +0000

    get cells that lie under PROW lines

--- Scripts/modules/extract_metric.R
+++ Scripts/modules/extract_metric.R
@@ -15,8 +15,8 @@ prw <- filter_distance(obj = final_acc_loc[[1]],
                        method = 'buffer')
 
 # example sdm metric
-plot(agg_rank$error_metric)
-plot(st_geometry(prw), add = T)
+plot(agg_rank$error_metric, col = bpy.colors(100))
+plot(st_geometry(prw), add = T, col = 'red', pwd = 2)
 
 
 ## could get a grid at 100m to use to cut the prow...
@@ -28,37 +28,18 @@ plot(sm_gr)
 plot(st_geometry(sm_gr))
 plot(st_geometry(prw), add = T)
 
-test_split <- st_split(prw, st_combine(sm_gr))
-dim(test_split)
-dim(prw)
-plot(test_split$geometry)
-plot(test_split['ROW_TYPE'])
-plot(test_split[1])
-plot(prw[1])
+# get the cells that the PROW cross
+prw_intersect <- apply(st_intersects(sm_gr, prw, sparse = FALSE), 1, any)
 
-plot(st_combine(sm_gr))
-
-BB <- st_cast(sm_gr, "MULTILINESTRING", group_or_split=FALSE)
-## Break LINESTRING A into segments by using:
-## - st_intersection() to find points at which lines features intersect
-## - st_buffer() to convert points to tiny polygons with some width
-## - st_difference() to break line up into sections not overlapping tiny polygons
-C <- st_difference(prw, st_buffer(st_intersection(prw,BB), dist=1e-12))
-plot(C)
-
-plot(sm_gr, col="grey")
-plot(st_cast(C, "LINESTRING"), lwd=3, add=TRUE)
-
-plot(agg_rank$error_metric)
+# plot the cells touching the PROW
+plot(sm_gr[prw_intersect, ], key.pos = NULL)
 plot(st_geometry(prw), add = T)
-plot((sm_gr), border = 'red', add = T)
 
-grid_intersect <- apply(st_intersects(uk_grid, uk_map, sparse = FALSE), 1, any)
+ggplot() +
+  geom_sf(data = sm_gr[prw_intersect,], aes(fill = error_metric)) +
+  geom_sf(data = prw) +
+  scale_fill_viridis(direction = -1)
 
-transect = raster::extract(agg_rank$error_metric, prw, 
-                           along = TRUE, cellnumbers = TRUE, df = F)
-transect_df = purrr::map_dfr(transect, as_data_frame, .id = "ID")
-transect_coords = xyFromCell(agg_rank$error_metric, transect_df$cell)
 
 
 
