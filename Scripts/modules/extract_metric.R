b5160c86b16017a9a9adebc67d9ade0c9913e578
Thomas Mondain-Monval <thoval@ceh.ac.uk>
2021-02-24 16:52:18 +0000

    Edited function to get the intersected grids between the metric and the shape_object so that it hopefully reduces the amount of overall time that it takes to run the function. Need to run some tests to check that this actually improves the time it takes.

--- Scripts/modules/extract_metric.R
+++ Scripts/modules/extract_metric.R
@@ -24,11 +24,15 @@ extract_metric <- function(metric,
     stop('Both objects must be of class "sf". Is your metric a raster? Convert it using "stars" package.')
   }
   
+  # first get the intersected boxes to reduce processing time
+  intersects <- apply(st_intersects(metric, shape_obj, sparse = FALSE), 1, any)
+  intersected_metric <- metric[intersects,]
+
   print('#####     cropping     #####')
   # crop the shape object to the grid of the metric
   # maybe parallelize??
-  cropped <- lapply(c(1:length(metric[[2]])), FUN = function(x){
-    loop_crop <- st_crop(shape_obj, st_bbox(metric[x,]))
+  cropped <- lapply(c(1:length(intersected_metric[[2]])), FUN = function(x){
+    loop_crop <- st_crop(shape_obj, st_bbox(intersected_metric[x,]))
     return(loop_crop)
   })
   
@@ -46,25 +50,33 @@ extract_metric <- function(metric,
 
 
 
-# greenspace/prow files
-# prow
-plot(st_geometry(final_acc_loc[[1]]))
+###############       Development/testing below
 
+## First get the error metric raster from the 'making_recommendations' workflow
+# need to convert this raster layer to an sf object because
+# it is easier to combine/manipulate two sf objects than to combine
+# raster and sf objects
+# can do this through the 'stars' package
+library(stars)
+
+plot(agg_rank) ## use the aggregated sum prob+variation across all species
+
+# convert raster to an sf object
+# convert to its own function?
+stars_rast <- st_as_stars(agg_rank$error_metric) # only interested in the error metric
+sm_gr <- st_as_sf(stars_rast, as_points = FALSE, merge = FALSE)
+st_crs(sm_gr) <- 27700
+
+
+## Now get the PROW files from 'find_accessible_areas' workflow
+# prow files from the 'final_acc_loc' object
+plot(st_geometry(final_acc_loc[[1]])) ## prow
+
+# crop to match area that we were interested from the raster
 prw <- filter_distance(obj = final_acc_loc[[1]],
                        # location = location,
                        distance = 2000,
                        method = 'buffer')
-
-# example sdm metric
-plot(agg_rank$error_metric, col = bpy.colors(100))
-plot(st_geometry(prw), add = T, col = 'red', pwd = 2)
-
-
-## could get a grid at 100m to use to cut the prow...
-## convert raster to an sf object
-t <- st_as_stars(agg_rank$error_metric)
-sm_gr <- st_as_sf(t, as_points = FALSE, merge = FALSE)
-st_crs(sm_gr) <- 27700
 
 plot(sm_gr)
 plot(st_geometry(sm_gr))
@@ -74,11 +86,6 @@ plot(st_geometry(prw), add = T)
 prw_intersect <- apply(st_intersects(sm_gr, prw, sparse = FALSE), 1, any)
 
 # plot the cells touching the PROW
-plot(sm_gr[prw_intersect, ], key.pos = NULL)
-plot(st_geometry(prw), add = T)
-## don't overlap properly - not sure why...
-
-# try with ggplot - looks nicer and more easily customisable
 ggplot() +
   geom_sf(data = sm_gr[prw_intersect,], aes(fill = error_metric)) +
   geom_sf(data = prw) +
@@ -87,35 +94,42 @@ ggplot() +
 
 # use st_join to get the error metric attached to the 
 # PROW file
-t <- st_join(prw, sm_gr,
-             largest = FALSE,
-             join = st_intersects)
+prw_errgrid <- st_join(prw, sm_gr,
+                       largest = FALSE,
+                       join = st_intersects)
 
-# test plot
+# plot to try
 ggplot() +
   # geom_sf(data = sm_gr[prw_intersect,], aes(fill = error_metric)) +
-  geom_sf(data = t, aes(colour = error_metric)) +
+  geom_sf(data = prw_errgrid, aes(colour = error_metric)) +
   # scale_fill_viridis(option = 'B') +
   scale_colour_viridis(option = 'B') +
   coord_sf(datum = sf::st_crs(27700))
 
+## this is good but there is only one error metric (not sure which...?)
+## associated with each PROW segment - NOT what we want.
 
 
-#### try and crop the prow file
+####    Try to crop the prow file with the error metric grid
 
-# first get the intersected files
-er_int <- sm_gr[prw_intersect,]
-plot(er_int)
+# the error metric is an sf object with each square a different geometry 
+# polygon in each row
+sm_gr
 
-# get bbox of one geometry
-st_bbox(er_int[1,])
+# get bbox of one geometry - need this to do the cropping
+st_bbox(sm_gr[1,])
 
-test_crop <- st_crop(prw, st_bbox(er_int[1,]))
-plot(st_geometry(test_crop))
+# crop the PROW that are in the first square in the sm_gr object
+test_crop <- st_crop(prw, st_bbox(sm_gr[1,]))
+plot(st_geometry(test_crop)) ## actually is nothing in the first square...
 
+
+### to crop for all boxes use a for loop/lapply statement
+
+# initiate list
 cropped_prw <- vector("list", length = length(sm_gr[[2]]))
 
-# loop
+# the loop
 for(i in 1:length(sm_gr[[2]])) {
   
   loop_crop <- st_crop(prw, st_bbox(sm_gr[i,]))
@@ -124,18 +138,25 @@ for(i in 1:length(sm_gr[[2]])) {
 }
 
 # that worked
-plot(cropped_prw[[5]])
+plot(cropped_prw[[750]])
 
 # combine all
 crp_prw <- do.call('rbind', cropped_prw) # takes AGES
 plot(st_geometry(crp_prw)) ## looks okay
 crp_prw
+length(unique(crp_prw$Name)) ## the name is duplicated because of the cropping
 
-# join error_metric and cropped prow
+
+# now that the lines are cropped to the grid and then joined together
+# we can then find the error metric value that is associated with
+# any particular segment of the PROW
+# join error_metric and cropped prow using the st_join() function
+# after testing - using the st_within join argument is definitely the best 
 crp_prw_err <- st_join(crp_prw, sm_gr,
                        largest = FALSE,
                        join = st_within) ## st_within is the argument to use!
 
+# plot to see what these error metrics look like
 ggplot() +
   # geom_sf(data = sm_gr[prw_intersect,], aes(fill = error_metric)) +
   geom_sf(data = crp_prw_err, aes(colour = error_metric)) +
@@ -143,20 +164,24 @@ ggplot() +
   scale_colour_viridis(option = 'B') +
   coord_sf(datum = sf::st_crs(27700)) +
   theme_bw()
+## preeeettttyyyyyy cool
 
 
 
+####   Aaaannnnddd thhheeeenn   ####
 
-##### Do this with accessible areas
+# Try this same thing with accessible areas
 
 # crop sf object to region of interest
+## final_acc_loc[[2]] is the accessible greenspaces
 accs <- filter_distance(obj = final_acc_loc[[2]],
                         # location = location,
                         distance = 2000,
                         method = 'buffer')
 plot(st_geometry(accs), col = 'green') # all good
 
-## convert raster uncertainty layer to an sf object (same as above)
+## convert raster uncertainty layer to an sf object (same as above - just doing
+## for consistency)
 stars_rast <- st_as_stars(agg_rank$error_metric)
 sm_gr <- st_as_sf(stars_rast, as_points = FALSE, merge = FALSE)
 st_crs(sm_gr) <- 27700
@@ -176,6 +201,7 @@ ggplot() +
 #### try and crop the accessible area file
 
 # first get the intersected files
+# the loop and bining works much quicker with this
 er_accs_int <- sm_gr[accs_intersect,]
 plot(er_accs_int)
 
@@ -185,16 +211,16 @@ plot(st_geometry(test_crop))
 cropped_accs <- vector("list", length = length(er_accs_int[[2]]))
 
 # loop
-for(i in 1:length(er_accs_int[[2]])) {
+system.time(for(i in 1:length(er_accs_int[[2]])) {
   
   loop_crop <- st_crop(accs, st_bbox(er_accs_int[i,]))
   cropped_accs[[i]] <- loop_crop
   
-}
+})
 
 
 # combine all
-crp_accs <- do.call('rbind', cropped_accs) # takes AGES
+system.time(crp_accs <- do.call('rbind', cropped_accs)) # takes AGES
 plot(st_geometry(crp_accs)) ## looks okay
 crp_accs
 
