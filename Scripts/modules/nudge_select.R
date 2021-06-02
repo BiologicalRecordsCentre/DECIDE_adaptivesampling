4231bfd6454d11caf60a10156fb46d14e9f30258
Thomas Mondain-Monval <thoval@ceh.ac.uk>
2021-06-02 10:54:21 +0000

    Function can now select from above or below the cutoff and now uses a specified column in the data frame to determine the weights.

--- Scripts/modules/nudge_select.R
+++ Scripts/modules/nudge_select.R
@@ -10,28 +10,39 @@
 
 nudge_select <- function(nudge_df,
                          n = 15,
+                         cutoff = TRUE,
                          weight = TRUE,
                          weighting_column,
                          weight_inflation = 10) {
   
-  nudge_df <- nudge_df[nudge_df$above_cutoff == 1,]
-  print('!   Only selecting nudges from above the cutoff')
+  if(cutoff == TRUE){
+    
+    nudge_df_cut <- nudge_df[nudge_df$above_cutoff == 1,]
+    print('!   Only selecting nudges from above the cutoff')
+    
+  } else if(cutoff == FALSE) {
+    
+    nudge_df_cut <- nudge_df
+    print('!   Selecting nudges from above and below cutoff')
+    
+  }
+  
   
   if(weight == TRUE){
     
-    nudge_inds <- sample(1:dim(nudge_df)[1], 
+    nudge_inds <- sample(1:dim(nudge_df_cut)[1], 
                          n,
-                         prob = nudge_df[, weighting_column]^weight_inflation)
+                         prob = nudge_df_cut[, weighting_column]^weight_inflation)
     
   } else if(weight == FALSE){
     
-    nudge_inds <- sample(1:dim(nudge_df)[1], 
+    nudge_inds <- sample(1:dim(nudge_df_cut)[1], 
                          n)
     
     
   } else{ stop('!!!   Weights must either be TRUE or FALSE, more options will be implemented later   !!!')}
   
-  nudge_outs <- nudge_df[nudge_inds,]
+  nudge_outs <- nudge_df_cut[nudge_inds,]
   
   return(nudge_outs)
   
