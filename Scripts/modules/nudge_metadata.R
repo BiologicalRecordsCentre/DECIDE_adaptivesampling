99a186dde343fc0f6e680edcef1b1f7820db7426
Thomas Mondain-Monval <thoval@ceh.ac.uk>
2021-06-07 12:35:38 +0000

    added some names to look for in the sf data frames. This now works for all of England.

--- Scripts/modules/nudge_metadata.R
+++ Scripts/modules/nudge_metadata.R
@@ -8,23 +8,20 @@ access = final_acc_loc
 buffer = 200
 
 
-access_sub <- access[lengths(access) != 0]
+# remove NULL items in list and get each layer buffered
+shapes_list <- lapply(access[lengths(access) != 0], FUN = function(x) ((st_buffer(x, buffer))))
 
-
-# get each layer buffered
-shapes_list <- lapply(access_sub, FUN = function(x) ((st_buffer(x, buffer))))
-
-
-lapply(1:dim(tn_df)[1], FUN = function(n) { # I want to find which shapes each point is in
+out_nudge <- lapply(1:dim(tn_df)[1], FUN = function(n) { # I want to find which shapes each point is in
   
-  out <- lapply(1:length(shapes_list), FUN = function(x){
+  out_shape <- lapply(1:length(shapes_list), FUN = function(x){
     
     # find the shapes that the point falls within
     if(sf::st_within(tn_df[n,], shapes_list[[x]]) %>% lengths > 0){# does the point fall within any of the shape?
       
       shps_within <- shapes_list[[x]][sf::st_within(tn_df[n,], shapes_list[[x]], sparse = F),]
       
-      sw_df <- shps_within[,names(shps_within) %in% c('ROW_TYPE', 'function_', 'Descrip')] %>%
+      sw_df <- shps_within[,names(shps_within) %in% c('ROW_TYPE', 'function_', 'Descrip',
+                                                      'accessType', 'Name', 'fclass')] %>% ## need to find all other names
         mutate(lon = unlist(map(tn_df[n,]$geometry,1)),
                lat = unlist(map(tn_df[n,]$geometry,2)),
                geometry = NULL) %>%
@@ -40,11 +37,11 @@ lapply(1:dim(tn_df)[1], FUN = function(n) { # I want to find which shapes each p
     })
   
   
-  return(do.call(rbind, out))
+  return(do.call(rbind, out_shape))
   
 })
 
-
+do.call(rbind, out_nudge)
 
 # # # if the point falls within buffered region store its index, otherwise skip to next loop
 # # ifelse(any(int_ind), int_nudge <- tn_df[int_ind,], return(NULL))
